bdc962045f91559cf7138fd00af8862bde0ee53e
Nireeksha17 <56109493+Nireeksha17@users.noreply.github.com>
2023-04-16 22:02:19 +0530

    Delete Random Passoword.py

--- Random Passoword.py
+++
@@ -1,5 +0,0 @@
-import random
-passlen = int(input("Enter the length of your password"))
-characters_of_password ="abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
-p = "".join(random.sample(characters_of_password,passlen))
-print(p)
