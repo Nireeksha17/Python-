bf6a24e97d09b2755410de2c4abdea8eedb61de6
Nireeksha17 <56109493+Nireeksha17@users.noreply.github.com>
2023-04-19 14:57:34 +0530

    Update snake.py

--- games/snake.py
+++ games/snake.py
@@ -11,3 +11,9 @@ pygame.font.init()
 random.seed()
 
 #on 17-04-2023 
+#screen initiaization
+display_screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT),pygame.HWSURFACE)
+# hwsurface = hardware surface refers to using memory on the video card for storing
+# draws as opposed to main memory
+
+#Resources needed for the game [score_card,snake_food,
